6026cd8978dbd379a6a4fa6febe240e74ad11e32
JC443852 <manpreet757@yahoo.in>
2016-12-09 20:56:49 +1000

    new changes

---
+++ Prac03/functions.py
@@ -0,0 +1,15 @@
+__author__ = 'manpreet'
+def inch_to_meter(inch):
+    return inch * 0.0254
+print(inch_to_meter(48))
+
+tax = 0
+income = float(input("income:"))
+if income > 16000:
+    tax = (income-16000)*0.3
+print(tax)
+
+i=0
+for i in range(1,12,3):
+ print(i,end ='')
+print(i)
